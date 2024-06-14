bdb5e3d0f7ab5943a06e5d7c3ee9b134f7a39b32
MostafizurRahman199 <fardilshifat199@gmail.com>
2024-06-14 21:24:39 +0600

    navbar created

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className='bg-white h-[60px] rounded-lg m-4 flex justify-center items-center'>
+        <img src="/logos_firebase.svg" alt="logo firebase" />
+        <h1>Firebase Contact App</h1>
+    </div>
+  )
+}
---
+++ src/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCVnfsSXrCKaZmjFUVi0QR_B3ltP5DMmDY",
+  authDomain: "react-contact-app-2d28e.firebaseapp.com",
+  projectId: "react-contact-app-2d28e",
+  storageBucket: "react-contact-app-2d28e.appspot.com",
+  messagingSenderId: "466963429990",
+  appId: "1:466963429990:web:31f19d2ab0b1f9b640cba7"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app); 
